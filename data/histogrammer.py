b43a84156b11f2e35ede92dfec758d251f5d491d
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2015-11-03 11:00:07 -0500

    Normalized histogram

--- data/histogrammer.py
+++ data/histogrammer.py
@@ -1,6 +1,10 @@
 from PIL import Image
-import os
 import csv
+
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from ml_util import ml
+import numpy as np
 
 # default to current directory
 path = "screenshots/"
@@ -16,8 +20,9 @@ print "Found %d %s images" % (len(imgs), fileExt)
 histograms = [Image.open(path+img).histogram() for img in imgs]
 assert(len(histograms) == len(imgs))
 
+norm_histograms = [ml.normalizeData(np.array(h)) for h in histograms]
 
 with open('colorgram.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    for i in xrange(len(histograms)):
-        writer.writerow([imgs[i]] + histograms[i])
+    for i in xrange(len(norm_histograms)):
+        writer.writerow([imgs[i]] + norm_histograms[i].tolist())
