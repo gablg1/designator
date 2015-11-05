c715ad8545b280f0d82c5c1838d82af2e52d663b
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2015-11-05 07:52:59 -0500

    histogram of top 15k

--- data/histogrammer.py
+++ data/histogrammer.py
@@ -6,8 +6,21 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from ml_util import ml
 import numpy as np
 
+amount = 'top-15k'
 # default to current directory
-path = "screenshots/"
+CUT = True
+BIG = True
+# choose the screenshots directory
+if CUT and BIG:
+    path = "cut_screenshots/"
+elif CUT and not BIG:
+    path = "small_cut_screenshots/"
+elif not CUT and BIG:
+    path = 'screenshots/'
+elif not CUT and not BIG:
+    path = 'small_screenshots/'
+
+path = '%s/%s' % (amount, path)
 
 fileList = os.listdir(path)
 fileExt = ".png"
@@ -23,11 +36,15 @@ D = 768
 histograms = [Image.open(path+img).histogram()[:D] for img in imgs]
 assert(len(histograms) == len(imgs))
 print "Each histogram has %d elements" % len(histograms[0])
+print 'Normalizing them...'
 
 assert(np.sum(ml.normalizeData(np.array(histograms[0]))) == 1.)
 norm_histograms = [ml.normalizeData(np.array(h)) for h in histograms]
 
-with open('colorgram.csv', 'wb') as csvfile:
+to = path + 'colorgram.csv'
+with open(to, 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
     for i in xrange(len(norm_histograms)):
-        writer.writerow([imgs[i]] + norm_histograms[i].tolist())
+    	j = imgs[i].find('.')
+        rank, name = int(imgs[i][:j]), imgs[i][j+1:]
+        writer.writerow([rank, name] + norm_histograms[i].tolist())
