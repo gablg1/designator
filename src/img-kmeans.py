cde1359102819bce919accfa91909a4714a1ad99
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2015-11-05 08:43:35 -0500

    K means

--- src/img-kmeans.py
+++ src/img-kmeans.py
@@ -5,6 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+# Hack to import ml_util from the parent directory
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from ml_util import ml
 from data import data
 
 # gets Screenshots directory as string
