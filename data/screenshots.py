f44b30b1010b6a361543ec59888ba8ca817f3788
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2015-11-04 22:33:48 -0500

    100 websites in

--- data/screenshots.py
+++ data/screenshots.py
@@ -3,18 +3,21 @@ import sys
 from urlparse import urlparse
 import csv
 
-websites_file = 'top-10k.csv'
+
+amount = 'top-1k'
+websites_file = amount + '.csv'
 CUT = True
 if CUT:
     to_path = 'cut_screenshots'
 else:
 	to_path = 'screenshots'
+to_path = '%s/%s' % (amount, to_path)
 
 with open(websites_file, 'r') as csvfile:
     lines = csv.reader(csvfile, delimiter=',')
     for line in lines:
-    	assert(len(lines) == 2)
-    	ranking, url = line[0], line[1]
+        assert(len(line) == 2)
+    	ranking, url = int(line[0]), line[1]
 
         # URL can be malformed, so we use urlparse to make sure it becomes
         # nicely formatted
@@ -27,7 +30,7 @@ with open(websites_file, 'r') as csvfile:
             cut = '--cliprect 0x0x1366x768'
         else:
             cut = ''
-        cmd = "capturejs --uri '%s' %s --viewport 1366x768 --output %s/%s.png" % (website, cut, to_path, o.netloc)
+        cmd = "capturejs --uri '%s' %s -T 2000 --viewport 1366x768 --output %s/%d.%s.png" % (website, cut, to_path, ranking, o.netloc)
         print "Website number %d" % ranking
         print cmd
         os.system(cmd)
