d5ca6f9cc22a0020fd4f68f9fe0e80f7b253df50
Brian Arroyo <brianarroyo@college.harvard.edu>
2015-11-06 15:24:42 -0500

    changed testing algorithm to consider the ratio to the max value

--- src/tester.py
+++ src/tester.py
@@ -4,37 +4,39 @@ import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from copy import deepcopy
 import recommender
-"""
-expected recommender function signature is going to be recommendFromCluster(x, cluster): color, howMuch
-the test plan is currently:
-    given an cluster, separate into test and train sets
-    take an element from the test set, subtract out the mean color
-    our elements are going to be histograms of colors
-    send this modified image to the recommendFromCluster function.
-    take the recommendation and reapply it to the image, diff the newImage with the initial one and
-    the rmse will be the sum of the difference squared
-
-    the cluster will be a list of histograms
-"""
-
-highFactor = .2
-lowFactor = .1
 
 
-def tester(cluster, fractionTrain=.9):
+def tester(cluster, fractionTrain=.9, highFactor=.1):
+    """
+    Parameters:
+        cluster: an array of arrays
+        fractionTrain: a float specifying percent of data to use as train
+        highFactor: ratio of any given element to the largest element in the array
+    Returns:
+        the RMSE produced by removing a specific color, then adding it back
+
+    Algorithm:
+        Works by first finding the max value in the histogram and then trying to find the
+        index of the histogram that contains the value that is closest (in terms of a ration
+        with the max value) to highFactor. Removes this color, then passes it to recommender
+        to get back a value which it then adds to the testHistogram, then takes the rmse between
+        the original and the modified
+    """
     endTrain = round(cluster.shape[0] * fractionTrain)
     xTrain = cluster[0:endTrain,]
     xTest = cluster[endTrain:,]
     mse = 0.
+    prevDiff = 1000
+    index = None
     for testHist in xTest:
-        testSum = np.sum(testHist)
+        maxVal = np.amax(testHist)
         copiedHist = deepcopy(testHist)
         for i in xrange(len(testHist)):
-            if testHist[i]/testSum < .1:
-                continue
-            elif testHist[i]/testSum >= .2:
-                testHist[i]-= testSum * .2
-                break
+            ratio = np.fabs(testHist[i]/maxVal - highFactor)
+            if ratio < prevDiff:
+                index = i
+        assert(index != None)
+        testHist[i] = 0
         color, howMuch = recommender.recommendFromCluster(testHist, xTrain)
         testHist[color]+= howMuch
         diff = copiedHist - testHist
