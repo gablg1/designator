1191835ad5c0a865816ed5514c728e03bad2337e
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2015-11-20 12:55:57 -0500

    Initial Aff Prop (not working)

--- src/aff-prop.py
+++ src/aff-prop.py
@@ -14,35 +14,86 @@ from ml_util import poly_features
 from ml_util import simple_plot
 from data import data
 
+import config
 
-amount='top-15k'
-ranks, website_names, X = data.getBandHistograms(amount=amount, cut=True, big=True)
+amount=config.amount
+ranks, website_names, X = data.getBandHistograms(amount=amount, cut=config.cut, big=config.big)
 
 N, D = X.shape
 print "Each feature vector has dimension %d" % D
 print "Training on %d samples" % N
 
-numClusters = 20
-kmeans = KMeans(n_clusters = numClusters)
-clusters = kmeans.fit_predict(X)
-clusters.sort()
-assert(len(clusters) == N)
-websites = []
-for i in range(len(clusters)):
-    websites.append((clusters[i], website_names[i]))
-websites.sort()
-print websites
+def euclideanDistance(a, b):
+    return np.linalg.norm(a - b)
 
 
-#data.plotClusters(websites, clusters=numClusters, xFactor=75, yFactor=25)
+# Similarities matrix
+S = np.zeros((N, N))
+for i in xrange(N):
+    for j in xrange(N):
+        S[i, j] = -euclideanDistance(X[i], X[j])
 
-# Writes kmeans object to pickle
-to = amount + '-histogram-affprop'
-pickle_to = '../persist/%s.pkl' % to
-joblib.dump(kmeans, pickle_to)
+# Preference i p(i) = S(i,i)
+# This is the a priori suitability of point i to serve as an exemplar
+# We initialize all of them to the median
+median = np.median(S)
+for i in xrange(N):
+	S[i, i] = median
+print S
+# Responsabilities matrix
+R = np.zeros((N, N))
 
-# Writes clusters to csv
-with open(to + '.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    for i in xrange(len(clusters)):
-        writer.writerow([clusters[i], ranks[i], website_names[i]])
+# Availability matrix
+A = np.zeros((N, N))
+
+C = np.array(xrange(N))
+
+for epoch in xrange(10):
+    print epoch
+    # Responsibility updates
+    print 'R before'
+    print R
+    for i in xrange(N):
+        for k in xrange(N):
+            max_sum = 0
+            for j in xrange(N):
+                val = A[i, j] + S[j, i]
+                if j != k and val > max_sum:
+                    max_sum = val
+            R[i, k] = S[i, k] - max_sum
+    print 'R after'
+    print R
+
+    print 'A before'
+    print A
+    # Availability updates
+    for i in xrange(N):
+        for k in xrange(N):
+            term = 0
+            for j in xrange(N):
+                if j != i and j != k:
+                    term += max(0, R[j, k])
+
+            if i == k:
+                A[k, k] = term
+            else:
+                A[i, k] = min(0, R[k, k] + term)
+    print 'A after'
+    print A
+
+    # Cluster assignments
+    #RAp = R + A.T
+    for i in xrange(N):
+        argmax = 0
+        valmax = 0
+        for k in xrange(N):
+            val = R[i, k] + A[k, i]
+            if val > valmax:
+                valmax = val
+                argmax = k
+
+        #c = np.argmax(RAp[i, :])
+        C[i] = argmax
+        print i, C[i]
+
+print S
